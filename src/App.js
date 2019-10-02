a2afe480d99ed75a1418b249ccb0f6508d71f5af
frontendista <csdelreal@gmail.com>
2019-10-02 15:27:38 -0500

    using hooks instead of class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -8,34 +8,28 @@ import Container from 'react-bootstrap/Container'
 import ChartWrapper from './ChartWrapper'
 import GenderDropdown from './GenderDropdown'
 
-class App extends React.Component {
-	state = {
-		gender: 'men'
-	}
+function App() {
+	const [gender, setGender] = useState('men');
 
-	genderSelected = gender => this.setState({ gender })
-
-	render() {
-		return (
-			<div className="App">
-				<Navbar bg="light">
-					<Navbar.Brand>Charts &amp; Co.</Navbar.Brand>
-				</Navbar>
-				<Container>
-					<Row>
-						<Col xs={12}>
-							<GenderDropdown genderSelected={this.genderSelected} />
-						</Col>
-					</Row>
-					<Row>
-						<Col xs={12}>
-							<ChartWrapper gender={this.state.gender} />
-						</Col>
-					</Row>
-				</Container>
-			</div>
-		)
-	}
+	return (
+		<div className="App">
+			<Navbar bg="light">
+				<Navbar.Brand>The World's Tallest People</Navbar.Brand>
+			</Navbar>
+			<Container>
+				<Row>
+					<Col xs={12}>
+						<GenderDropdown genderSelected={setGender} gender={gender} />
+					</Col>
+				</Row>
+				<Row>
+					<Col xs={12}>
+						<ChartWrapper gender={gender} />
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	)
 }
 
 export default App;
