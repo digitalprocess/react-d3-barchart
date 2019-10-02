168117bfb47ccb3a45ed80bf44995a6e5846106e
frontendista <csdelreal@gmail.com>
2019-10-01 19:24:51 -0500

    adding multiple sets of data as well as transitions between datasets

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,62 +1,105 @@
 import * as d3 from 'd3'
 
-const url = 'https://udemy-react-d3.firebaseio.com/tallest_men.json'
 const MARGIN = {TOP: 10, RIGHT: 10, BOTTOM: 50, LEFT: 70}
 const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT // width of the "canvas"
 const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM // height of the "canvas"
 
 class D3Chart {
 	constructor(element) {
-		const svg = d3.select(element)
+		const vis = this
+
+		vis.svg = d3.select(element)
 			.append('svg')
 				.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
 				.attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 			.append('g')
 				.attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
-		d3.json(url).then(data => {
-			const y = d3.scaleLinear()
-				.domain([
-					d3.min(data, d => d.height) * 0.95,
-					d3.max(data, d => d.height)
-				])
-				.range([HEIGHT, 0])
+		vis.svg.append('text')
+			.attr('x', WIDTH / 2)
+			.attr('y', HEIGHT + 50)
+			.attr('text-anchor', 'middle')
+			.text("The world's tallest men")
 
-			const x = d3.scaleBand()
-				.domain(data.map(d => d.name))
-				.range([0, WIDTH])
-				.padding(0.4)
+		vis.svg.append('text')
+			.attr('x', -(HEIGHT / 2))
+			.attr('y', -50)
+			.attr('text-anchor', 'middle')
+			.text('Height in cm')
+			.attr('transform', 'rotate(-90)')
 
-			const xAxisCall = d3.axisBottom(x)
-			svg.append('g').call(xAxisCall)
-				.attr('transform', `translate(0, ${HEIGHT})`)
+		vis.xAxisGroup = vis.svg.append('g')
+			.attr('transform', `translate(0, ${HEIGHT})`)
 
-			const yAxisCall = d3.axisLeft(y)
-			svg.append('g').call(yAxisCall)
+		vis.yAxisGroup = vis.svg.append('g')
 
-			svg.append('text')
-				.attr('x', WIDTH / 2)
-				.attr('y', HEIGHT + 50)
-				.attr('text-anchor', 'middle')
-				.text("The world's tallest men")
+		Promise.all([
+			d3.json('https://udemy-react-d3.firebaseio.com/tallest_men.json'),
+			d3.json('https://udemy-react-d3.firebaseio.com/tallest_women.json'),
+		]).then(datasets => {
+			const [men, women] = datasets
+			let flag = true
 
-				svg.append('text')
-				.attr('x', -(HEIGHT / 2))
-				.attr('y', -50)
-				.attr('text-anchor', 'middle')
-				.text('Height in cm')
-				.attr('transform', 'rotate(-90)')
+			vis.data = men
+			vis.update()
 
-			const rects = svg.selectAll('react')
-				.data(data)
-
-			rects.enter().append('rect')
-				.attr('x', d => x(d.name))
-				.attr('y', d => y(d.height))
-				.attr('width', x.bandwidth)
-				.attr('height', d => HEIGHT - y(d.height))
-				.attr('fill', 'grey')
+			d3.interval(() => {
+				vis.data = flag ? men : women
+				vis.update()
+				flag =! flag
+			}, 1000)
 		})
+	}
+
+	update() {
+		const vis = this
+
+		const y = d3.scaleLinear()
+			.domain([
+				d3.min(vis.data, d => d.height) * 0.95,
+				d3.max(vis.data, d => d.height)
+			])
+			.range([HEIGHT, 0])
+
+		const x = d3.scaleBand()
+			.domain(vis.data.map(d => d.name))
+			.range([0, WIDTH])
+			.padding(0.4)
+
+		const xAxisCall = d3.axisBottom(x)
+			vis.xAxisGroup.transition().duration(500).call(xAxisCall)
+
+		const yAxisCall = d3.axisLeft(y)
+			vis.yAxisGroup.transition().duration(500).call(yAxisCall)
+
+		// DATA JOIN
+		const rects = vis.svg.selectAll('rect')
+			.data(vis.data)
+
+		// EXIT
+		rects.exit()
+		.transition().duration(500)
+			.attr('height', 0)
+			.attr('y', HEIGHT)
+			.remove()
+
+		// UPDATE
+		rects.transition().duration(500)
+			.attr('x', d => x(d.name))
+			.attr('y', d => y(d.height))
+			.attr('width', x.bandwidth)
+			.attr('height', d => HEIGHT - y(d.height))
+
+		// ENTER
+		rects.enter().append('rect')
+			.attr('x', d => x(d.name))
+			.attr('width', x.bandwidth)
+			.attr('fill', 'grey')
+			.attr('y', HEIGHT)
+			.transition().duration(500)
+				.attr('height', d => HEIGHT - y(d.height))
+				.attr('y', d => y(d.height))
+
 	}
 }
 
